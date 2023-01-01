1a5004b62319b874c8bb9a2409aa053d4f039aca
ednukros <isanmenarguez@gmail.com>
2023-01-01 16:27:56 +0100

    fix---controlling white space bug

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const input$$ = document.querySelector("input")
 const btn$$ = document.querySelector(".btn-add")
 const ul$$ = document.querySelector("ul")
+const pTasks$$ = document.querySelector(".pTasks")
 
 
 let counter;
@@ -10,9 +11,10 @@ let counter;
 /*FUNCIÓN DONDE CAPTURAMOS EL TEXTO DEL INPUT, GUARDAMOS EN UN LI, EL CUAL AÑADIMOS DENTRO DEL UL DEL LI CONTAINER*/
 function addNewItem() {
 
-    let text$$ = input$$.value;
-     const listContainer$$ = document.querySelector(".li-container")
+
+    const listContainer$$ = document.querySelector(".li-container")
     const li$$ = document.createElement("li");
+    let text$$ = input$$.value;
     li$$.textContent = text$$;
     listContainer$$.appendChild(ul$$);
     ul$$.appendChild(li$$);
@@ -22,12 +24,53 @@ function addNewItem() {
 
 }
 
-btn$$.addEventListener('click', (e) => {
 
+
+btn$$.addEventListener('click', validate);
+
+function validate(e) {
     e.preventDefault(); //con este metodo conseguimos que no se recargue la página cada vez que le damos al boton
-    addNewItem();
 
-});
+    let valid = true;
+    const nameError = document.querySelector("#nameError");
+
+    if (!input$$.value || input$$.value === " ") {
+        nameError.removeAttribute("hidden");
+        input$$.removeAttribute("valid");
+        input$$.setAttribute("class","invalid");
+        nameError.setAttribute("class","visible");
+        nameError.setAttribute("aria-hidden", false);
+        nameError.setAttribute("aria-invalid", true);
+    } else {
+        input$$.removeAttribute("invalid");
+        nameError.removeAttribute("visible");
+        nameError.setAttribute("class","hidden");
+        input$$.setAttribute("class","valid");
+        nameError.setAttribute("aria-hidden", true);
+        nameError.setAttribute("aria-invalid", false);
+        addNewItem();
+    }
+
+    return valid;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function removeBtn() {
@@ -45,7 +88,7 @@ function removeBtn() {
         ul$$.removeChild(item);
         counter--;
         statusTasks();
-        
+
 
 
     });
@@ -54,25 +97,33 @@ function removeBtn() {
 
 }
 
-function AlertEmptyInput() {
-
-    const div$$ = document.querySelector(".search") //seleccionamos
-    const p$$ = document.createElement("p"); //creamos p donde mostrar mensaje
-    p$$.textContent = "introduce un texto para añadir a tu lista"; //mostramos este mensaje
-    div$$.appendChild(p$$); //hacemos realidad :)
-    p$$.setAttribute("class", "parragraph-invalid") //añadimos una clase a nuestro p
 
 
-}
+// function AlertEmptyInput() {
+
+// //    if(p$$ === textInvalid){
+// //    const textInvalid = "introduce un texto para añadir a tu lista";
+
+// //    const div$$ = document.querySelector(".search") //seleccionamos
+// //    const p$$ = document.createElement("p"); //creamos p donde mostrar mensaje
+// //    p$$.textContent = textInvalid ; //mostramos este mensaje
+// //    div$$.appendChild(p$$); //hacemos realidad :)
+// //    p$$.setAttribute("class", "parragraph-invalid") //añadimos una clase a nuestro p
+//    
+// //    }
+
+
+
+// }
 
 function statusTasks() {
     counter = ul$$.children.length
-    
-    const pTasks$$ = document.querySelector(".pTasks")
-    if(!counter>0){
-        pTasks$$.textContent="No tienes tareas pendientes.";
-    }else{
-       
-        pTasks$$.textContent="";
+
+
+    if (!counter > 0) {
+        pTasks$$.textContent = "No tienes tareas pendientes.";
+    } else {
+
+        pTasks$$.textContent = "";
     }
 }
